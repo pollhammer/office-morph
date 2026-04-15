9849f96d2a32508925bfd7c3de31d94d20691c60
Manuel Pollhammer <109719423+pollhammer@users.noreply.github.com>
2026-04-15 11:23:09 +0200

    The core processing engine

--- FolderConverter.ps1
+++ FolderConverter.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    OFFICE-MORPH 2026 - Core Conversion Engine
+    OFFICE-MORPH 1.1 - Core Conversion Engine
     
 .DESCRIPTION
     Converts legacy .doc, .xls, and .ppt files into modern XML formats.
